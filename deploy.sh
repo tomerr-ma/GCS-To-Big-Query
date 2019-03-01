af101674a30aa1667df516fe43f08a1657282c17
tomerr-ma <43787455+tomerr-ma@users.noreply.github.com>
2019-03-01 22:31:48 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
-BUCKET_NAME="my_cloudflarelogs_gcp_storage_bucket"
+BUCKET_NAME="cloudflare-ma"
 DATASET="cloudflare_logs"
-TABLE="cloudflare_logs"
+TABLE="cloudflare_moonactive_net_"
 
 gcloud functions deploy gcsbq \
     --runtime nodejs8 \
-    --trigger-resource $BUCKET_NAME \
-    --trigger-event google.storage.object.finalize \
+    --trigger-http \
     --memory=1024MB \
-    --set-env-vars DATASET=$DATASET,TABLE=$TABLE,SCHEMA="./schema.json"
+    --set-env-vars DATASET=$DATASET,TABLE=$TABLE,BUCKET_NAME=$BUCKET_NAME,SCHEMA="./schema.json"
