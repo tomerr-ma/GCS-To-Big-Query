c77293f0a070ee4b5cb4bfc502f7d4607db70d1b
tomerr-ma <43787455+tomerr-ma@users.noreply.github.com>
2019-03-01 22:28:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,33 +7,35 @@ async function gcsbq (file, context) {
   const _schema = require(process.env.SCHEMA)
 
   const datasetId = process.env.DATASET
-  
+
   var date = new Date();
   var day = date.getDate();
-  
+
   var dayStr = "" + day;
   if (day < 10) {
 	dayStr = "0" + day
   }
-  
+
   var monthStr = "" + (date.getMonth()+1);
-  
+
   if ((date.getMonth()+1) < 10) {
 	monthStr = "0" + monthStr
   }
-  
+
   var todayStr = date.getFullYear() + monthStr + dayStr;
-  
+
   const tableId = (process.env.TABLE + '_' + todayStr);
+  const bucketName = process.env.BUCKET_NAME
+
   console.log("table name: " + tableId);
-  
+
   const bigquery = new BigQuery()
 
   const storage = new Storage()
 
-  console.log(`Starting job for ${file.name}`)
 
-  const filename = storage.bucket(file.bucket).file(file.name)
+
+  const filename = storage.bucket(bucketName).file("logs/"+todayStr+"/*")
 
   /* Configure the load job and ignore values undefined in schema */
   const metadata = {
@@ -49,9 +51,11 @@ async function gcsbq (file, context) {
   await dataset.get({ autoCreate: true }, (e, dataset, res) => {
     if (e) console.log(e)
     dataset.table(tableId).get({ autoCreate: true }, (e, table, res) => {
-      table.load(filename, metadata)
+      table.load(filename, metadata).then(function(data) {
+        table.query(query)
+        bucket.deleteFiles(function(err) {});
+       });
     })
   })
 }
-
 exports.gcsbq = gcsbq
