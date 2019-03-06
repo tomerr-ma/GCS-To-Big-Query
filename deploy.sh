ec75128571baa05317c260599ab97ee96a44b6ea
tomerr-ma <43787455+tomerr-ma@users.noreply.github.com>
2019-03-06 15:28:43 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-BUCKET_NAME="cloudflare-ma"
-DATASET="cloudflare_logs"
+BUCKET_NAME="MYBAUCKET"
+DATASET="MYDATASET"
 TABLE="cloudflare_logs"
 
-gcloud functions deploy gcsbq \
+gcloud functions deploy gcsbq --project aviv-playground\
     --runtime nodejs8 \
-    --trigger-resource $BUCKET_NAME \
-    --trigger-event google.storage.object.finalize \
+    --timeout 540 \
+    --trigger-http \
     --memory=1024MB \
-    --set-env-vars DATASET=$DATASET,TABLE=$TABLE,SCHEMA="./schema.json"
+    --set-env-vars DATASET=$DATASET,TABLE=$TABLE,BUCKET_NAME=$BUCKET_NAME,SCHEMA="./schema.json"
